1ca3a82698f28e29d59f787398392719b70b6a0e
fafaman <fabrice.lebas@gmail.com>
2009-12-13 13:37:34 +0100

    release procedure.

--- autotool/autotool.sh
+++ autotool/autotool.sh
@@ -5,9 +5,4 @@ cp autotool/Makefile.am . &&\
 aclocal &&\
 autoconf &&\
 autoheader &&\
-automake --add-missing &&\
-./configure &&\
-make clean &&\
-make &&\
-date ; ls -la meterec
-
+automake --add-missing
