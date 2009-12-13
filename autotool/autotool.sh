07bf99f52aa7299f990a7468fe8a2082d4133a67
fafaman <fabrice.lebas@gmail.com>
2009-12-13 13:42:45 +0100

    updated autotool instalation script.

--- autotool/autotool.sh
+++ autotool/autotool.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+make clean
+ 
+rm Makefile aclocal.m4 Makefile.am Makefile.in config config.h configure config.h.in config.log config.status configure.in depcomp install-sh missing stamp-h1
+rm config.h.in~
+rm -rf autom4te.cache
+
 cp autotool/configure.in . &&\
 cp autotool/Makefile.am . &&\
 aclocal &&\
