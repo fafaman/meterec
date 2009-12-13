4505f92895c56567d79d913856884472bba8cfbe
fafaman <fabrice.lebas@gmail.com>
2009-12-13 21:24:52 +0100

    Fine tuning release procedure

--- autotool/autotool.sh
+++ autotool/autotool.sh
@@ -3,7 +3,6 @@
 make clean
  
 rm Makefile aclocal.m4 Makefile.am Makefile.in config config.h configure config.h.in config.log config.status configure.in depcomp install-sh missing stamp-h1
-rm config.h.in~
 rm -rf autom4te.cache
 
 cp autotool/configure.in . &&\
@@ -11,4 +10,5 @@ cp autotool/Makefile.am . &&\
 aclocal &&\
 autoconf &&\
 autoheader &&\
-automake --add-missing
+automake --add-missing --copy &&\
+exit 0
--- release.sh
+++ release.sh
@@ -2,9 +2,14 @@
 
 RELEASE=0.5
 
-./autotool/autotool.sh &&\
-cd ../ &&\
-gtar -zcvf meterec-rel-$RELEASE.tgz \
+echo Relasing meterec-$RELEASE
+echo =====================
+
+pushd ../
+
+mv meterec-dev meterec-$RELEASE
+
+gtar -zcvf meterec-$RELEASE.tgz \
 meterec-$RELEASE/meterec.c \
 meterec-$RELEASE/meterec-init-conf \
 meterec-$RELEASE/NEWS \
@@ -15,12 +20,17 @@ meterec-$RELEASE/COPYING \
 meterec-$RELEASE/Makefile.in \
 meterec-$RELEASE/Makefile.am \
 meterec-$RELEASE/configure \
-meterec-$RELEASE/configure.in
+meterec-$RELEASE/configure.in \
 meterec-$RELEASE/depcomp \
 meterec-$RELEASE/missing \
 meterec-$RELEASE/install-sh \
 meterec-$RELEASE/config.h.in \
 meterec-$RELEASE/aclocal.m4 \
-meterec-$RELEASE/autom4te.cache/* \
-cd meterec-$RELEASE
+meterec-$RELEASE/autom4te.cache/* 
 
+mv meterec-$RELEASE meterec-dev
+
+popd
+
+echo =====================
+echo Done in ../meterec-$RELEASE.tgz
