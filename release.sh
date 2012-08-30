6ebb6945d4f40169ccfa41641a4cd4e699344395
Fabrice Lebas <fabrice@kotoubas.net>
2012-08-30 16:08:45 +0200

    releasing v0.9.1

--- release.sh
+++ release.sh
@@ -13,21 +13,20 @@ tar -zcvf meterec-$RELEASE.tgz \
 meterec-$RELEASE/*.c \
 meterec-$RELEASE/*.h \
 meterec-$RELEASE/meterec-init-conf \
-#meterec-$RELEASE/meterec-pass-thru \
 meterec-$RELEASE/README \
 meterec-$RELEASE/NEWS \
 meterec-$RELEASE/TODO \
 meterec-$RELEASE/INSTALL \
 meterec-$RELEASE/AUTHORS \
 meterec-$RELEASE/COPYING \
-#meterec-$RELEASE/Makefile.in \
+meterec-$RELEASE/Makefile.in \
 meterec-$RELEASE/Makefile.am \
+meterec-$RELEASE/config.h.in \
+meterec-$RELEASE/configure.in \
 meterec-$RELEASE/configure \
-#meterec-$RELEASE/configure.in \
 meterec-$RELEASE/depcomp \
 meterec-$RELEASE/missing \
 meterec-$RELEASE/install-sh \
-#meterec-$RELEASE/config.h.in \
 meterec-$RELEASE/aclocal.m4 \
 
 mv meterec-$RELEASE meterec-dev
